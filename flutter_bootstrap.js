cde985f5f7a2bb99c412fded06ab6ffaf28f8ea6
daigiaherhar <tungdoan1477@gmail.com>
2025-05-01 16:15:36 +0000

    Deploying to portfolio-page from @ daigiaherhar/portfolio_thanh_tung@329ec7628886a03f939e09722fc1513f6f19c9e1 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "907507412"
+    serviceWorkerVersion: "3469470612"
   }
 });
